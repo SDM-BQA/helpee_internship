63dd2e549af1719ea9d5900b0a30e548e7f5afa1
SDQ-BQA <sdmbqa420@gmail.com>
2025-06-23 17:21:11 +0530

    day 6

--- day 6/src/App.jsx
+++ day 6/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   Route,
   createBrowserRouter,
@@ -6,16 +7,45 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout/>}>
-      <Route index element={<HomePage/>} />
-    </Route>
-  )
-);
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch("http://localhost:8000/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const deleteJob = async (id) => {
+   const res = await fetch(`http://localhost:8000/jobs/${id}`, {
+      method: "DELETE"
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
--- day 6/src/components/JobListings.jsx
+++ day 6/src/components/JobListings.jsx
@@ -1,17 +1,39 @@
-import jobs from "../jobs.json";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
-const JobListings = () => {
-    const recentJobs = jobs.slice(0, 3)
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' :'http://localhost:8000/jobs'
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log(error);
+      }finally{
+        setLoading(false)
+      }
+    };
+    fetchJobs()
+  }, []);
+
+  // const jobList = isHome ? jobs.slice(0, 3) : jobs
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
+            {loading && <Spinner/>}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
-           <JobListing key={job.id} job={job}/>
+          {jobs.map((job) => (
+            <JobListing key={job.id} job={job} />
           ))}
         </div>
       </div>
